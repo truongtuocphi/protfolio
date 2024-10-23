9e9eba19b47272e61e9edc93e24d91e18b8da91b
truongphi <truongphi754@gmail.com>
2024-10-23 09:09:03 +0700

    feat: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>Home Page I&apos;m Truong Tuoc Phi</div>;
+  return (
+    <div className="w-full h-screen grid grid-cols-7 items-center">
+      <div className="col-span-2"></div>
+      <div className="col-span-3 w-full bg-blue-500"></div>
+      <div className="col-span-2"></div>
+    </div>
+  );
 }
