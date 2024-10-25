46b24a86d02047ffb8e2698ee5ac816d8944481e
truongphi <truongphi754@gmail.com>
2024-10-25 09:30:18 +0700

    feat: update

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,40 +1,36 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { usePathname } from "next/navigation"; // Sử dụng usePathname từ next/navigation
-import Loading from "@/components/Loading"; // Component loading của bạn
+import { usePathname } from "next/navigation";
+import Loading from "@/components/Loading";
 
 const PageWrapper = ({ children }: { children: React.ReactNode }) => {
   const [loading, setLoading] = useState(false);
-  const pathname = usePathname(); // Lấy pathname hiện tại
+  const pathname = usePathname();
 
   useEffect(() => {
-    // Kích hoạt loading khi bắt đầu thay đổi URL
     const handleStart = () => {
       setLoading(true);
     };
 
-    // Tắt loading khi hoàn tất chuyển trang
     const handleComplete = () => {
       setLoading(false);
     };
 
-    // Mỗi khi pathname thay đổi, ta kích hoạt loading
     handleStart();
 
-    // Tắt loading sau khi thay đổi hoàn tất
     const timer = setTimeout(() => {
       handleComplete();
-    }, 500); // Đặt thời gian 500ms để mô phỏng quá trình load (tùy chỉnh theo nhu cầu)
+    }, 500);
 
     return () => {
-      clearTimeout(timer); // Xóa timer khi component unmount
+      clearTimeout(timer);
     };
-  }, [pathname]); // Theo dõi sự thay đổi của URL (pathname)
+  }, [pathname]);
 
   return (
     <>
-      {loading && <Loading />} {/* Hiển thị loading khi đang tải */}
+      {loading && <Loading />}
       {children}
     </>
   );
