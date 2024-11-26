ce538930fc49cc80be45069826fa16cc747a01d0
truongphi <truongphi754@gmail.com>
2024-11-26 13:53:51 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,9 @@
-import Image from "next/image";
+import ProfileHero from "@/components/home/ProfileHero";
 
 export default function Home() {
   return (
-    <div className="w-full h-[calc(100vh-112px)] grid grid-cols-1 md:grid-cols-8 items-center text-center ">
-      <div className="flex flex-col justify-center md:justify-end col-span-2">
-        <p className="responsive-font-xl text-end">Phi Truong</p>
-        <div
-          className="flex flex-col items-end text-base"
-          style={{ fontFamily: '"PP Neue Montreal", sans-serif' }}
-        >
-          <span>Live and work in Ho Chi Minh City</span>
-          <span>Available for a full-time position</span>
-        </div>
-      </div>
-
-      <div className="col-span-4 flex items-center justify-center">
-        <div className="w-[550px] h-[550px] p-2 border-[3px] border-blue-500 rounded-full -z-20">
-          <div className="relative w-full h-full bg-blue-500 rounded-full ">
-            <Image
-              src={"/images/user.png"}
-              className="absolute w-auto h-auto"
-              fill
-              alt={"Logo"}
-              sizes="(max-width: 700px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div className="flex flex-col justify-center md:justify-end col-span-2">
-        <span className="responsive-font-base text-left">
-          Fresher Front-end Web Developer
-        </span>
-      </div>
-    </div>
+    <>
+      <ProfileHero />
+    </>
   );
 }
